7b19992c943201422d815d914ba3a4c0f1a1b0db
Seb Ben M'Barek <sebastien.benmbarek@normanandsons.com>
2022-11-22 21:03:35 +0000

    Fixed a few things
    - added type to VITE env variables for autocomplete
    - Rename env variables for Frame and Default view
    - Rename Frame to TopNavigation
    - Moved packages from src/ to topNavigation/src
    - Added —inspect so I can auto attach only when running the watch script (instead of always)
    - Fixed the production builds for tailwindcss
    - Fixed the public assets in production builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    /**
+     * For some reason the line below needs to be commented otherwise the npm run complie fails
+     */
+    tailwindcss: {
+      config: path.join(__dirname, 'tailwind.config.js'),
+    },
     autoprefixer: {},
   },
 };
