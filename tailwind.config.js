15ab01e2c120e30d1b49c4d7e417ce35f1d4d7c1
Seb Ben M'Barek <sebastien.benmbarek@normanandsons.com>
2022-11-28 19:16:33 +0000

    Fix tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [path.join(__dirname, './packages/**/*.{html,js,ts,jsx,tsx}')],
+  content: [
+    path.join(__dirname, './packages/renderer/**/src/*.{js,ts,jsx,tsx}'),
+  ],
   theme: {
     extend: {},
   },
