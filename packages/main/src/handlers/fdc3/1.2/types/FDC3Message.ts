9424ae563e15192ab5a77b816dff3e992a639568
Rob Moffat <rob@kite9.com>
2022-12-02 11:04:24 +0000

    Reverted types

---
+++ packages/main/src/handlers/fdc3/1.2/types/FDC3Message.ts
@@ -0,0 +1,34 @@
+import { FDC3App } from '/@/handlers/fdc3/1.2/types/FDC3Data';
+import { Context, TargetApp } from 'fdc3-1.2';
+import { RuntimeMessage } from '../../../runtimeMessage';
+
+export interface FDC3Message extends RuntimeMessage {
+  name?: string;
+  intent?: string;
+  selected?: FDC3App;
+  context?: Context;
+}
+
+export interface FDC3MessageData {
+  id?: string;
+  eventId?: string;
+  context?: Context;
+  name?: string;
+  intent?: string;
+  source?: string; //the viewId (internal instance identifier) of the sender of the message
+  channel?: string; //name of source/related channel
+  contextType?: string;
+  instanceId?: string;
+  ts?: number; //timestamp (for pending contexts/intents)
+  target?: TargetApp;
+  channelId?: string; //to do : refactor with channel prop
+  type?: string;
+  restoreOnly?: boolean;
+  selectedIntent?: string;
+  selected?: FDC3App;
+}
+
+export interface FDC3Response {
+  error?: string;
+  data: any;
+}
