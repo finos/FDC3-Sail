457c2e9c662cdcc1430347ad6d607ccc56bfbea7
Nicholas Kolba <nkolba@gmail.com>
2023-04-26 07:47:47 +0100

    cleanup

--- packages/renderer/channelPicker/src/Picker.tsx
+++ packages/renderer/channelPicker/src/Picker.tsx
@@ -3,20 +3,21 @@ import React from 'react';
 import { Grid } from '@mui/material';
 
 import PickerItem from './components/PickerItem';
-import { channels } from "../../../main/src/system-channels";
-
+import { systemChannels } from '../../../main/src/handlers/fdc3/lib/systemChannels';
 
 export const Picker = () => {
-
-
-    return (
-        <div id="container">
-        <Grid container spacing={1}>
-            {channels.map(channel => 
-                <PickerItem key={channel.id} altColor={channel.displayMetadata.color2} color={channel.displayMetadata.color} id={channel.id}></PickerItem>
-            )}
-        </Grid>
-        </div>
-
-    );
+  return (
+    <div id="container">
+      <Grid container spacing={1}>
+        {systemChannels.map((channel) => (
+          <PickerItem
+            key={channel.id}
+            altColor={channel.displayMetadata.color2}
+            color={channel.displayMetadata.color}
+            id={channel.id}
+          ></PickerItem>
+        ))}
+      </Grid>
+    </div>
+  );
 };
