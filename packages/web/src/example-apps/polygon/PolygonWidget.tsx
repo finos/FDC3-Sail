441ce5e9c5deb9518a92dac8ce77b2f74eaf1275
Rob Moffat <rob@kite9.com>
2025-03-14 12:22:51 +0000

    Added polygon news app

---
+++ packages/web/src/example-apps/polygon/PolygonWidget.tsx
@@ -0,0 +1,58 @@
+// TradingViewWidget.jsx
+import { getAgent } from "@finos/fdc3"
+import { useEffect, useRef, memo, useState } from "react"
+import { PolygonMode } from "./common"
+import { newsMode } from "./modes/news"
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
+const API_KEY = "9FpXO2gTxbbtGsrmNk8dROv9H7zpDrYo"
+
+const MODES: PolygonMode[] = [newsMode]
+
+export const PolygonWidget = ({ mode }: { mode: string }) => {
+  const container: any = useRef()
+  const modeProps = MODES.find((m) => m.name === mode) ?? MODES[0]
+
+  const [state, setState] = useState(modeProps.initialState)
+  const [data, setData] = useState(modeProps.initialData)
+
+  useEffect(() => {
+    getAgent().then((fdc3) => {
+      modeProps.intents.forEach((intent) => {
+        fdc3.addIntentListener(intent.name, (context) => {
+          const newState = intent.function(context, state)
+          setState(() => newState)
+          console.log("new state", newState)
+        })
+      })
+
+      modeProps.listeners.forEach((listener) => {
+        fdc3.addContextListener(listener.name, (context) => {
+          const newState = listener.function(context, state)
+          setState(() => newState)
+          console.log("new state", newState)
+        })
+      })
+    })
+  }, [state])
+
+  useEffect(() => {
+    const call = modeProps.endpoint(state, API_KEY)
+    fetch(call).then(async (response) => {
+      console.log("response", response)
+      const data = await response.json()
+      console.log("data", data)
+      setData(() => data)
+    })
+  }, [state])
+
+  return (
+    <div id="polygon-widget" ref={container}>
+      {modeProps.stateRenderer(state)}
+      {modeProps.dataRenderer(data)}
+    </div>
+  )
+}
+
+export default memo(PolygonWidget)
