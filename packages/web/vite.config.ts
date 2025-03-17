00d57bd8b3f29f4f4fca156927262f3b88dc1ee7
Rob Moffat <rob@kite9.com>
2025-03-17 12:04:41 +0000

    shadow dom styles still missing

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,9 @@
-import { fileURLToPath, URL } from "url"
 import { defineConfig } from "vite"
 import glob from "glob"
 export default defineConfig({
     build: {
+        cssMinify: false,   // makes the css easier to read
+        sourcemap: true,    // provide souremap for prod debug
         rollupOptions: {
             input: glob.sync("html/**/*.html"),
         },
